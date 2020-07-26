f2bc657592d01f47b233f488cf2de88300c22b5f
Charlie U <dzcharlie@gmail.com>
2020-07-26 18:30:07 -0400

    Load preferences.json for connection details and listen for !gradient

--- bot.py
+++ bot.py
@@ -15,10 +15,36 @@ if not 'DISCORD_TOKEN' in prefs:
     print('DISCORD_TOKEN missing from preferences.json!  Exiting...')
     sys.exit()
 
-client = Discord.client()
+if not 'DISCORD_GUILD' in prefs:
+    print('DISCORD_GUILD missing from preferences.json!  Exiting...')
+    sys.exit()
+
+DISCORD_TOKEN = prefs['DISCORD_TOKEN']
+DISCORD_GUILD = prefs['DISCORD_GUILD']
+
+client = discord.Client()
 
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
+    
+    guild = discord.utils.get(client.guilds, name=DISCORD_GUILD)
+    if guild is None:
+        print(f'{client.user} does not belong to {DISCORD_GUILD}!  Exiting...')
+        await client.close()
+        return
+
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name} (id: {guild.id})'
+    )
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content == '!gradient':
+        await message.channel.send('You want a gradient?  Here you go!')
 
 client.run(DISCORD_TOKEN)
