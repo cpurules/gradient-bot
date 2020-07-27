750519ac3df8b9f768702657603f4c678fe0ac37
Charlie U <dzcharlie@gmail.com>
2020-07-27 09:19:39 -0400

    Rework to use bot commands

--- bot.py
+++ bot.py
@@ -1,11 +1,17 @@
-import discord
 import json
 import lib.gradient
 import os
 import random
 import sys
 
-# ready preference
+from discord.ext import commands
+
+def loadWordLists():
+    global adjectives, nouns
+    adjectives = open('english-adjectives.txt').read().splitlines()
+    nouns = open('english-nouns.txt').read().splitlines()
+
+# get preference
 if not os.path.exists('preferences.json'):
     print('preferences.json missing!  Exiting...')
     sys.exit()
@@ -17,74 +23,54 @@ if not 'DISCORD_TOKEN' in prefs:
     print('DISCORD_TOKEN missing from preferences.json!  Exiting...')
     sys.exit()
 
-if not 'DISCORD_GUILD' in prefs:
-    print('DISCORD_GUILD missing from preferences.json!  Exiting...')
-    sys.exit()
-
 DISCORD_TOKEN = prefs['DISCORD_TOKEN']
-DISCORD_GUILD = prefs['DISCORD_GUILD']
 
-client = discord.Client()
+bot = commands.Bot(command_prefix='!')
 
 requesters = []
 
 adjectives = []
 nouns = []
+loadWordLists()
 
-def loadWordList():
-    global adjectives, nouns
-    adjectives = open('english-adjectives.txt').read().splitlines()
-    nouns = open('english-nouns.txt').read().splitlines()
-
-loadWordList()
-
-@client.event
+@bot.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
-    
-    guild = discord.utils.get(client.guilds, name=DISCORD_GUILD)
-    if guild is None:
-        print(f'{client.user} does not belong to {DISCORD_GUILD}!  Exiting...')
-        await client.close()
+    print(f'{bot.user.name} is connected!')
+
+@bot.command(name='gradient', help='Generate a random gradient and name it')
+async def gradient(ctx):
+    requester = ctx.author
+    msg = ctx.message
+
+    if(requester.id in requesters):
+        print(f'{requester} already has a request in')
         return
-
-    print(
-        f'{client.user} is connected to the following guild:\n'
-        f'{guild.name} (id: {guild.id})'
-    )
-
-@client.event
-async def on_message(message):
-    if message.author == client.user or message.author.id in requesters:
-        if(message.author.id in requesters):
-            print(f'{message.author} already has a request in')
-        return
-
-    if message.content == '!gradient' or message.content == '!g':
-        print(f'Generating gradient for {message.author}')
-        requesters.append(message.author.id)
-
-        await getGradient(message.id)
-        print(f'Gradient {message.id}.png generated, sending')
-
-        gradientName = random.choice(adjectives).capitalize() + ' ' + random.choice(nouns).capitalize()
-
-        gradientFile = discord.File(f'{message.id}.png', filename='gradient.png')
-
-        try:
-            await message.channel.send(file = gradientFile, content = (f'{message.author.mention} here you go!  I call this one **' + gradientName + '**'))
-        except:
-            print(f'Error sending gradient for {message.author}')
-        finally:
-            os.remove(f'{message.id}.png')
-            requesters.remove(message.author.id)
     
-    if message.content == '!reload':
-        print(f'Reloading word list!')
-        loadWordList()
+    print(f'Generating gradient for {requester}')
+    requesters.append(requester.id)
 
+    await lib.gradient.createRandomGradient(filename=msg.id)
+    print(f'Gradient {msg.id}.png generated, sending')
 
-async def getGradient(f):
-    lib.gradient.createRandomGradient(filename=f)
+    gradientName = random.choice(adjectives).capitalize() + ' ' + random.choice(nouns).capitalize()
 
-client.run(DISCORD_TOKEN)
+    gradientFile = discord.File(f'{msg.id}.png', filename='gradient.png')
+
+    try:
+        await ctx.send(file = gradientFile, content = (f'{requester.mention} here you go!  I call this one **' + gradientName + '**'))
+    except:
+        print(f'Error sending gradient for {requester}')
+    finally:
+        os.remove(f'{msg.id}.png')
+        requesters.remove(requester.id)
+
+@bot.command(name='g', help='Shortcut for !gradient')
+async def g(ctx):
+    await gradient.invoke(ctx)
+
+@bot.command(name='reload', help='Reload word lists for names')
+async def reload(ctx):
+    print(f'Reloading word list!')
+    loadWordList()
+
+bot.run(DISCORD_TOKEN)
